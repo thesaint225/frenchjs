20ded45132f10483e8ee130002f15a16cc2d32b0
henrymiessan <miessanhenri@gmail.com>
2024-05-21 00:22:44 +0000

    working on the single post

---
+++ components/Statistic.jsx
@@ -0,0 +1,230 @@
+"use client";
+import { Plus } from "lucide-react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useState } from "react";
+import slugify from "slugify";
+import parse from "html-react-parser";
+
+const Statistic = () => {
+  const [owner, setOwner] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [content, setContent] = useState("");
+  const [slug, setSlug] = useState("");
+
+  const [submitted, setSubmitted] = useState(false);
+
+  console.log(owner);
+  console.log(title);
+  console.log(description);
+  console.log(content);
+
+  const handleTitle = (e) => {
+    const newTitle = e.target.value;
+    setTitle(newTitle);
+    const autoSlug = slugify(newTitle, { lower: true });
+    setSlug(autoSlug);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const teamPayload = {
+      owner,
+      title,
+      description,
+      content,
+    };
+    console.log("Payload:", teamPayload);
+    setSubmitted(true);
+    setOwner("");
+    setTitle("");
+    setDescription("");
+    setContent("");
+
+    // send data over the server
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/blogs`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(teamPayload),
+      });
+      const data = await res.json();
+      console.log("response back", data);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
+
+  // Custom Tool Bar
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["link", "color", "image"],
+      [{ "code-block": true }],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "link",
+    "indent",
+    "image",
+    "code-block",
+    "color",
+  ];
+
+  return (
+    <div>
+      <h2 className="text-4xl text-center font-semibold py-4">
+        Create New Blog
+      </h2>
+      <div className="grid grid-cols-1 lg:grid-cols-2 p-8 gap-4">
+        <div className="w-full max-w-3xl p-5 my-6 bg-white border border-gray-200 rounded-lg shadow mx-auto">
+          <h2 className="text-3xl font-bold border-b border-gray-400 pb-2 mb-5 ">
+            Blog Editor
+          </h2>
+          <form onSubmit={handleSubmit}>
+            <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+              <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                <label
+                  htmlFor="title"
+                  className="block text-sm font-medium leading-6 text-gray-900 mb-2 "
+                >
+                  Blog Title
+                </label>
+                <div className="mt-2">
+                  <input
+                    onChange={handleTitle}
+                    type="text"
+                    value={title}
+                    name="title"
+                    id="title"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6"
+                    placeholder="Type the Blog title"
+                  />
+                </div>
+              </div>
+              {/* Slug */}
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="slug"
+                  className="block text-sm font-medium leading-6 text-gray-900 mb-2 "
+                >
+                  Blog Slug
+                </label>
+                <div className="mt-2">
+                  <input
+                    onChange={(e) => setSlug(e.target.value)}
+                    type="text"
+                    value={slug}
+                    name="slug"
+                    id="slug"
+                    autoComplete="slug"
+                    className="block w-full rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6"
+                    placeholder="Generated Slug"
+                    readOnly
+                  />
+                </div>
+              </div>
+              {/* Description */}
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="description"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Blog Description
+                </label>
+                <textarea
+                  id="description"
+                  rows="4"
+                  onChange={(e) => setDescription(e.target.value)}
+                  value={description}
+                  className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-purple-500 focus:border-purple-500 "
+                  placeholder="Write your thoughts here..."
+                ></textarea>
+              </div>
+              {/* Content */}
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="content"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Blog Content
+                </label>
+                <ReactQuill
+                  theme="snow"
+                  value={content}
+                  onChange={setContent}
+                  modules={modules}
+                  formats={formats}
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-purple-700 rounded-lg focus:ring-4 focus:ring-purple-200 dark:focus:ring-purple-900 hover:bg-purple-800"
+            >
+              <Plus className="w-5 h-5 mr-2" />
+              <span>Create Blog Post</span>
+            </button>
+          </form>
+        </div>
+        {/* Blog View */}
+        <div className=" blog-view w-full max-w-3xl p-8 my-6 bg-white border border-gray-200 rounded-lg shadow mx-auto">
+          <h2 className="text-3xl font-bold border-b border-gray-400 pb-2 mb-5 ">
+            Blog View
+          </h2>
+          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+            {/* Title */}
+            <div className="sm:col-span-2">
+              <h2 className="block text-sm font-medium leading-6 text-gray-900 mb-2 ">
+                Blog Title
+              </h2>
+              <div className="mt-2">
+                <p className="text-2xl font-bold">{title}</p>
+              </div>
+            </div>
+            {/* Slug */}
+            <div className="sm:col-span-2">
+              <h2 className="block text-sm font-medium leading-6 text-gray-900 mb-2 ">
+                Blog Slug
+              </h2>
+              <div className="mt-2">
+                <p>{slug}</p>
+              </div>
+            </div>
+            {/* Description */}
+            <div className="sm:col-span-2">
+              <h2 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Blog Description
+              </h2>
+              <p>{description}</p>
+            </div>
+            <div className="sm:col-span-full">
+              <h2 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Blog Content
+              </h2>
+              {parse(content)}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Statistic;
