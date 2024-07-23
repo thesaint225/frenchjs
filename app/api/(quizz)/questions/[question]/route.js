90bd8d814d75e4cb35197fd439627dffab71509a
henrymiessan <miessanhenri@gmail.com>
2024-07-23 15:53:41 +0000

    Add ability to delete  question title and options in DELETE request

--- app/api/(quizz)/questions/[question]/route.js
+++ app/api/(quizz)/questions/[question]/route.js
@@ -70,3 +70,53 @@ export const PATCH = async (request, context) => {
     );
   }
 };
+
+export const DELETE = async (request, context) => {
+  const questionId = context.params.question;
+  try {
+    const { searchParams } = new URL(request.url);
+    const blogId = searchParams.get("blogId");
+    if (!blogId || !Types.ObjectId.isValid(blogId)) {
+      return new Response(JSON.stringify({ message: "blog Id not found" }), {
+        status: 400,
+      });
+    }
+    if (!questionId || !Types.ObjectId.isValid(questionId)) {
+      return new Response(
+        JSON.stringify({ message: "question id not found" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    await connecteDb();
+
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      return new Response(JSON.stringify({ message: "blog not found" }), {
+        status: 404,
+      });
+    }
+    const question = await Question.findOne({ _id: questionId });
+    if (!question) {
+      return new Response(
+        JSON.stringify({ message: "question not found " }, { status: 404 })
+      );
+    }
+    const deletedQuestion = await Question.findByIdAndDelete(questionId);
+    return new Response(
+      JSON.stringify({
+        message: "question successfully deleted",
+        question: deletedQuestion,
+      }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response(
+      JSON.stringify({ message: "error occurred while deleting question " }),
+      { status: 400 }
+    );
+  }
+};
